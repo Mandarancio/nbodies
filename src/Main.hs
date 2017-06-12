d7a8b2be9cd4c666301badacf44b5ca086370c89
Martino Ferrari <manda.mgf@gmail.com>
2017-06-12 17:29:25 +0200

    Cleaned code and addded comments

--- src/Main.hs
+++ src/Main.hs
@@ -11,41 +11,34 @@ import Data.List
 import Data.IORef
 import System.IO
 
-
-
 import Control.Monad
-import Control.Monad.State
 
 import Foreign.C.Types
 import SDL.Vect
 import SDL.Primitive
 import qualified SDL
--- import Control.Monad.IO.Class (MonadIO, liftIO)
 
-
+-- screen information
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (800, 600)
 
 cx, cy :: Double
 (cx, cy) = (400, 300)
 
+
+-- few colors
 black :: SDL.Primitive.Color
 black = V4 0 0 0 255
 
 white :: SDL.Primitive.Color
 white = V4 255 255 255 255
 
-count :: State Int Int
-count = do
-    c <- get
-    put (c+1)
-    return (c+1)
-
-
-g = -1
+-- Gravitational Constant
+g = 1
+-- Simulation interval
 dT = 1e-1
 
-
+-- function to render all bodies
 renderAll :: SDL.Renderer -> [Body] -> IO ()
 renderAll r [] = do return ()
 renderAll r (Body{pos=(Vec2D x y), mass=_, mom=_}:rest) = do
@@ -54,33 +47,45 @@ renderAll r (Body{pos=(Vec2D x y), mass=_, mom=_}:rest) = do
   renderAll r rest
   return ()
 
+
+-- Entry point
 main :: IO ()
 main = do
+  -- initialize SDL
   SDL.initialize [SDL.InitVideo]
+  -- create Window
   window <- SDL.createWindow "SDL Tutorial" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+  -- create Renderer
   renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
 
+  -- show Windo
   SDL.showWindow window
-  screenSurface <- SDL.getWindowSurface window
+
   -- initialize bodies
   bodies <- newIORef [Phys.Body Phys.zeroVec 100 Phys.zeroVec, Phys.Body (Phys.Vec2D 0 100) 0.1 (Phys.Vec2D 1 0),  Phys.Body (Phys.Vec2D 0 200) 0.1 (Phys.Vec2D 0.7 0)]
-
+  -- main loop
   let
     loop = do
+      -- retrive events
       events <- SDL.pollEvents
+      -- check if quit action performed
       let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
-      SDL.rendererDrawColor renderer SDL.$= black
+
       -- clear screen
+      SDL.rendererDrawColor renderer SDL.$= black
       SDL.clear renderer
+
       -- update bodies position and speed
       modifyIORef bodies (Phys.simulate g dT)
       -- render bodies
       readIORef bodies >>= (renderAll renderer)
-      -- display bodies
+
+      -- display renderer
       SDL.present renderer
       unless quit loop
   loop
 
+  -- delete and quit
   SDL.destroyWindow window
   SDL.quit
 
--- src/Phys.hs
+++ src/Phys.hs
@@ -1,3 +1,5 @@
+{-Simple Physics module.-}
+
 module Phys
   (Vec2D(..)
   , Body(..)
@@ -9,45 +11,53 @@ module Phys
   , acc
   ) where
 
+-- 2D Vector
+data Vec2D = Vec2D Double Double deriving (Show, Eq)
 
-data Vec2D = Vec2D Double Double
-
+-- null Vector
 zeroVec = Vec2D 0 0
 
-instance Show Vec2D where
-  show (Vec2D x y) = "v("++(show x)++", "++(show y)++")"
-
-instance Eq Vec2D where
-  (==) (Vec2D a b) (Vec2D c d) = (a == c) && (b == d)
-
+-- basic numerical operations between Vector
 instance Num Vec2D where
   (+) (Vec2D a b) (Vec2D c d) = Vec2D (a + c) (b + d)
   (-) (Vec2D a b) (Vec2D c d) = Vec2D (a - c) (b - d)
   (*) (Vec2D a b) (Vec2D c d) = Vec2D (a * c) (b * d)
   abs (Vec2D a b) = Vec2D (abs a) (abs b)
-  signum (Vec2D a b) = Vec2D (a/sqrt(a*a + b*b)) (b/sqrt(a*a + b*b))
-  fromInteger a = Vec2D (fromInteger a ::Double) 0
+  signum (Vec2D a b) = Vec2D (signum a) (signum b)
+  fromInteger a = Vec2D (fromInteger a ::Double) (fromInteger a ::Double)
 
+-- scalar vector operations
+-- multiplication double vector
 (.*) :: Double -> Vec2D -> Vec2D
 (.*) x (Vec2D a b) = Vec2D (x*a) (x*b)
 
-(./) :: Vec2D -> Double -> Vec2D
-(./) (Vec2D a b) x = Vec2D (a/x) (b/x)
+-- multiplication vector double
+(*.) :: Vec2D -> Double-> Vec2D
+(*.) (Vec2D a b) x = Vec2D (x*a) (x*b)
 
+-- division double vector
+(./) :: Double -> Vec2D -> Vec2D
+(./) x (Vec2D a b) = Vec2D (x/a) (x/b)
+
+-- division vector double
+(/.) :: Vec2D -> Double -> Vec2D
+(/.) (Vec2D a b) x = Vec2D (a/x) (b/x)
+
+-- vector magnetude
 magnetude :: Vec2D -> Double
 magnetude (Vec2D a b) = sqrt(a*a + b*b)
 
-data Body = Body {pos::Vec2D,mass::Double,mom::Vec2D}
+-- celestial body type
+data Body = Body {pos::Vec2D,mass::Double,mom::Vec2D} deriving Show
 
-instance Show Body where
-  show (Body{pos=p, mass=m,mom=s}) = "b(p: "++(show p)++", m: "++(show m)++", s: "++(show s)++")"
-
+-- compute the acceleration between 2 bodies
 acc :: Double ->  Body -> Body -> Vec2D
 acc g (Body{pos=p1,mass=m1,mom=s1}) (Body{pos=p2,mass=_,mom=_}) | p1 == p2 =  zeroVec
 acc g (Body{pos=p1,mass=m1,mom=_}) (Body{pos=p2,mass=m2,mom=_}) =
-  let dist = (p1-p2)
+  let dist = (p2-p1)
   in (g*m2/(magnetude dist)^3) .* dist
 
+-- compute new position and speed of a body in a certain universe of [Body]
 computeStep :: Double -> Double -> [Body] -> Body -> Body
 computeStep _ _ [] b = b
 computeStep g dT bodies b@(Body{pos = p, mass = m, mom = s}) =
@@ -55,5 +65,6 @@ computeStep g dT bodies b@(Body{pos = p, mass = m, mom = s}) =
       newPos = (dT .* newSpeed) + p
   in Body{pos=newPos, mass=m, mom = newSpeed}
 
+-- simulate an iteration of a certain universe of [Body]
 simulate :: Double -> Double -> [Body] -> [Body]
 simulate g dT bodies = map (computeStep g dT bodies) bodies
